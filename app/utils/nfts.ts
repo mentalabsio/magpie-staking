d8ff06e9f858e9241f874f7958bd2c398206622f
ellagoris <eduardofidera@gmail.com>
2022-06-12 18:36:08 -0300

    feat: add trycatch

--- app/utils/nfts.ts
+++ app/utils/nfts.ts
@@ -1,13 +1,13 @@
-import { Connection, PublicKey } from "@solana/web3.js"
-import { TOKEN_PROGRAM_ID } from "@solana/spl-token"
-import axios from "axios"
-import { programs } from "@metaplex/js"
+import { Connection, PublicKey } from "@solana/web3.js";
+import { TOKEN_PROGRAM_ID } from "@solana/spl-token";
+import axios from "axios";
+import { programs } from "@metaplex/js";
 
-import { NFT } from "@/hooks/useWalletNFTs"
+import { NFT } from "@/hooks/useWalletNFTs";
 
 const {
   metadata: { Metadata },
-} = programs
+} = programs;
 
 export async function getNFTMetadata(
   mint: string,
@@ -15,18 +15,18 @@ export async function getNFTMetadata(
   pubkey?: string
 ): Promise<NFT | undefined> {
   try {
-    const metadataPDA = await Metadata.getPDA(mint)
-    const onchainMetadata = (await Metadata.load(conn, metadataPDA)).data
-    const externalMetadata = (await axios.get(onchainMetadata.data.uri)).data
+    const metadataPDA = await Metadata.getPDA(mint);
+    const onchainMetadata = (await Metadata.load(conn, metadataPDA)).data;
+    const externalMetadata = (await axios.get(onchainMetadata.data.uri)).data;
 
     return {
       pubkey: pubkey ? new PublicKey(pubkey) : undefined,
       mint: new PublicKey(mint),
       onchainMetadata,
       externalMetadata,
-    }
+    };
   } catch (e) {
-    console.log(`failed to pull metadata for token ${mint}`)
+    console.log(`failed to pull metadata for token ${mint}`);
   }
 }
 
@@ -34,13 +34,13 @@ export async function getNFTMetadataForMany(
   tokens: any[],
   conn: Connection
 ): Promise<NFT[]> {
-  const promises: Promise<NFT | undefined>[] = []
-  tokens.forEach((token) =>
+  const promises: Promise<NFT | undefined>[] = [];
+  tokens.forEach(token =>
     promises.push(getNFTMetadata(token.mint, conn, token.pubkey))
-  )
-  const nfts = (await Promise.all(promises)).filter((n) => !!n)
+  );
+  const nfts = (await Promise.all(promises)).filter(n => !!n);
 
-  return nfts
+  return nfts;
 }
 
 /**
@@ -51,22 +51,26 @@ export async function getNFTsByOwner(
   owner: PublicKey,
   conn: Connection
 ): Promise<NFT[]> {
-  const tokenAccounts = await conn.getParsedTokenAccountsByOwner(owner, {
-    programId: TOKEN_PROGRAM_ID,
-  })
+  try {
+    const tokenAccounts = await conn.getParsedTokenAccountsByOwner(owner, {
+      programId: TOKEN_PROGRAM_ID,
+    });
 
-  const tokens = tokenAccounts.value
-    .filter((tokenAccount) => {
-      const amount = tokenAccount.account.data.parsed.info.tokenAmount
+    const tokens = tokenAccounts.value
+      .filter(tokenAccount => {
+        const amount = tokenAccount.account.data.parsed.info.tokenAmount;
 
-      return amount.decimals === 0 && amount.uiAmount === 1
-    })
-    .map((tokenAccount) => {
-      return {
-        pubkey: tokenAccount.pubkey,
-        mint: tokenAccount.account.data.parsed.info.mint,
-      }
-    })
+        return amount.decimals === 0 && amount.uiAmount === 1;
+      })
+      .map(tokenAccount => {
+        return {
+          pubkey: tokenAccount.pubkey,
+          mint: tokenAccount.account.data.parsed.info.mint,
+        };
+      });
 
-  return await getNFTMetadataForMany(tokens, conn)
+    return await getNFTMetadataForMany(tokens, conn);
+  } catch (e) {
+    console.log(e);
+  }
 }
